1741e9aab87e12b37b34a32d6eb6de29517442c5
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-01-10 09:12:50 -0600

    Added instagram data to site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,18 @@ module.exports = {
         path: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-instagram',
+      username: 'gatsbyjs',
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
